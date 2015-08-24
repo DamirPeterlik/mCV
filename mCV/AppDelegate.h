12dcaae04515c3b51e013724e0fec260f1234cf4
DamirPeterlik <you@example.com>
2015-08-24 10:40:36 +0200

    First commit... 24.8.2015.

---
+++ mCV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mCV
+//
+//  Created by Damir Peterlik on 24/08/15.
+//  Copyright (c) 2015 Damir Peterlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mCV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mCV
+//
+//  Created by Damir Peterlik on 24/08/15.
+//  Copyright (c) 2015 Damir Peterlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
